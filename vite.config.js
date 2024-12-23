0111c557b6992da9fc8f4595755ed96f7ff351e1
FionaJores <fionajores10@gmail.com>
2024-12-24 00:07:13 +0530

    Fixed base URL for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // Ensures the output is placed in the 'dist' folder
+    outDir: 'build', 
   },
-  base: '/Crypto_Place/', // Correct base URL for GitHub Pages
+  base: '/Crypto_Place/', 
 });
