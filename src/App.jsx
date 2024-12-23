a7a6e8304f310bdd2178ceb5bf1901fdf192be95
FionaJores <fionajores10@gmail.com>
2024-12-23 23:39:42 +0530

    Fixed base URL for GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div className='app'>
         <Navbar/>
-        <Routes>
+        <Routes basename='/Crypto_Place'>
           <Route path='/' element={<Home/>}/>
           <Route path='/coin/:coinId' element={<Coin/>}/>
         </Routes>
