91304126bec841ac8e9d97af90331a01d3121055
FionaJores <fionajores10@gmail.com>
2024-12-23 23:50:46 +0530

    Fixed base URL for GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
-import {Routes,Route} from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Coin from './pages/Coin/Coin'
 import Footer from './components/Footer/Footer'
+
 const App = () => {
   return (
     <div className='app'>
-        <Navbar/>
+      <BrowserRouter basename='/Crypto_Place'>
+        <Navbar />
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/coin/:coinId' element={<Coin/>}/>
+          <Route path='/' element={<Home />} />
+          <Route path='/coin/:coinId' element={<Coin />} />
         </Routes>
-        <Footer/>
+        <Footer />
+      </BrowserRouter>
     </div>
   )
 }
